415e77af45b8af82d5c97edc4c91d9c638807d41
Иванаев Станислав Владимирович <ivanaev@skbkontur.ru>
2024-05-14 19:01:48 +0500

    update projects to .NET 8, update MongoDB client

--- Tests/TestMongoDatabase.cs
+++ Tests/TestMongoDatabase.cs
@@ -8,7 +8,7 @@ namespace Tests
         public static IMongoDatabase Create()
         {
             var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://localhost:27017";
+                                        ?? "mongodb://localhost:27017?maxConnecting=100";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
